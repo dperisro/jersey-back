5b86285515879a6ddd9f6ef02e8f8ff396f5311b
Dani Peris <dperisro@everis.com>
2014-06-12 18:27:03 +0200

    Testing

--- jersey-back/jersey-back-core/src/test/java/com/mdp/jersey/back/books/BaseJUnit.java
+++ jersey-back/jersey-back-core/src/test/java/com/mdp/jersey/back/books/BaseJUnit.java
@@ -1,11 +1,15 @@
 package com.mdp.jersey.back.books;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +20,24 @@ import com.mdp.jersey.back.books.dao.BookDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath*:module-context.xml")
+@Ignore
 public class BaseJUnit {
 
 	protected static Logger logger = Logger.getLogger(BaseJUnit.class);
 
+	protected final int id = 0;
+	protected final String image = "https://assets-cdn.github.com/images/spinners/octocat-spinner-32.gif";
+	protected final String title = "BookTitle";
+	protected final String author = "everis";
+	protected final double price = 5.99;
+	protected final String link = "/api/v1/books/42";
+	protected Book book;
+
 	@Autowired
-	BookDao bookDao;
+	protected BookDao bookDao;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-
 	}
 
 	@AfterClass
@@ -34,6 +46,11 @@ public class BaseJUnit {
 
 	@Before
 	public void setUp() throws Exception {
+		book = new Book(id);
+		book.setImage(image);
+		book.setTitle(title);
+		book.setAuthor(author);
+		book.setPrice(price);
 	}
 
 	@After
@@ -43,7 +60,21 @@ public class BaseJUnit {
 	@Test
 	public void testMinimal() throws Exception {
 		Assert.assertTrue(true);
-		bookDao.getAllBooks();
+	}
+
+	protected List<Book> insertBooks(int count) {
+		List<Book> books = new ArrayList<Book>();
+		for (int index = 1; index <= count; index++) {
+			Book book = new Book(index);
+			book.setTitle(title + "" + index);
+			book.setAuthor(author);
+			book.setImage(image);
+			book.setPrice(price + index);
+			book.setLink("/api/v1/books/" + index);
+			bookDao.saveBook(book);
+			books.add(book);
+		}
+		return books;
 	}
 
 }
